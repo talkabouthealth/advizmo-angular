8e26d5ac9c8c8e90bea361dadcd1190e00d1264d
magan-maestro <magan@maestro.com>
2020-04-07 02:57:34 +0530

    Initial Angular set up with Plaid link Button Integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'finance-app';
+  public access_token: string;
+  ngOnInit() {
+
+  }
+  ngOnDestroy() {
+
+  }
+  onPlaidSuccess($event: any) {
+    console.log('onPlaidSuccess executed');
+    console.log($event, '$event');
+    if ($event !== undefined && $event.token !==undefined) {
+      this.access_token = $event.token;
+      console.log(this.access_token, 'access_token');
+      // Call Backend API sending access token
+    } 
+  }
+  onPlaidExit($event: any) {
+    console.log('onPlaidExit executed');
+  }
+  onPlaidLoad($event: any) {
+    console.log('onPlaidLoad executed');
+  }
+  onPlaidEvent($event: any) {
+    console.log('onPlaidEvent executed');
+  }
+  onPlaidClick($event: any) {
+    console.log('onPlaidClick executed');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxPlaidLinkModule } from "ngx-plaid-link";
 
 @NgModule({
   declarations: [
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPlaidLinkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
